6d6d7626dc7979b859e83e5fcabf938c1c3df4b9
Capitan Pinto <santiagopfisica@gmail.com>
2019-10-24 18:23:14 +0000

    users can be remembered by browser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_224805) do
+ActiveRecord::Schema.define(version: 2019_10_24_163951) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_10_23_224805) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
